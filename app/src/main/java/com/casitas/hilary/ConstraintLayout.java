a47fb007370996c272ee95c954f833c21e061818
Hilary Casitas <hilary.casitas@gmail.com>
2018-09-08 21:17:56 +0800

    version5

---
+++ app/src/main/java/com/casitas/hilary/ConstraintLayout.java
@@ -0,0 +1,14 @@
+package com.casitas.hilary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name3);
+        setContentView(R.layout.constraint_layout);
+    }
+}
---
+++ app/src/main/java/com/casitas/hilary/RelativeLayout.java
@@ -0,0 +1,14 @@
+package com.casitas.hilary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name2);
+        setContentView(R.layout.relative_layout);
+    }
+}
