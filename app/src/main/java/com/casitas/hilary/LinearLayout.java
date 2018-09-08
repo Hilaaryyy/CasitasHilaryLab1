9bfde32630d8390a906d14af5c164afb57dc9cd3
Hilary Casitas <hilary.casitas@gmail.com>
2018-09-08 19:40:02 +0800

    version5

--- app/src/main/java/com/casitas/hilary/LinearLayout.java
+++ app/src/main/java/com/casitas/hilary/LinearLayout.java
@@ -3,11 +3,14 @@ package com.casitas.hilary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LinearLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint_layout);
+        setTitle(R.string.app_name1);
+        setContentView(R.layout.linear_layout);
+
+
     }
 }
